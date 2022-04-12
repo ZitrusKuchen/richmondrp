01837a4f560e37c7f0ea1caac970f852d22a06cc
ZitrusKuchen <luis.salzburg9@gmail.com>
2022-04-12 22:07:30 +0200

    lelcommit

---
+++ commands/ticket/create.js
@@ -0,0 +1,41 @@
+const Command = require('../../structures/Command');
+const config = require('../../config.json');
+const Discord = require('discord.js');
+
+module.exports = class extends Command {
+    constructor(...args) {
+      super(...args, {
+        name: 'ticket',
+        aliases: ["create", "setup"],
+        description: `Create A Ticket sent to a channel.`,
+        category: 'Ticket',
+        cooldown: 3,
+        userPermission: ['MANAGE_GUILD']
+      });
+    }
+
+    async run(message, args) {
+
+      
+        let channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
+
+        if(!channel){
+            channel = message.channel;
+        };
+        
+
+        let ticketEmbed = new Discord.MessageEmbed()
+       .setTitle(`Ticket System`)
+       .setColor(config.color || '#36393f')
+       .setTimestamp()
+       .setDescription(`Bitte reagiere ${config.emoji || '🎟️'} zum erstellen eines Tickets`)
+
+
+
+        channel.send(ticketEmbed).then(m => {
+        m.react(config.emoji || '🎟️');
+        });
+
+
+      }
+};
